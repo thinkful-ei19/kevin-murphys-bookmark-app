24c4868384d0c748570dab7e9268f8f53bab0400
midimurphdesigns <midimurphdesigns@gmail.com>
2018-03-01 15:47:50 -0700

    added item.js

--- scripts/item.js
+++ scripts/item.js
@@ -1,2 +1,22 @@
 'use strict';
-/* global */
+/* global cuid */
+
+const Item = (function(){
+  const validateName = function(name) {
+    if (!name) throw new TypeError('Name must not be blank');
+  };
+
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name,
+      adding: false,
+      expanded: false,
+    };
+  };
+
+  return {
+    validateName,
+    create,
+  };
+}());
